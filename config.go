9afda2bb66f8cd875ccb342562a6a31c406b34a3
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-04-30 02:36:26 +0300

    :white_check_mark: Add empty NewConfig function

--- config.go
+++ config.go
@@ -2,3 +2,8 @@ package daffodil
 
 // Config holds configuration values for building a Daffodil
 type Config struct{}
+
+// NewConfig initializes a Config struct
+func NewConfig() (*Config, error) {
+	return &Config{}, nil
+}
