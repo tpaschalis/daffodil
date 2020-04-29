8b0ebf2ce97ce9a61ea65195761d5cce466cbd5b
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-04-30 02:36:04 +0300

    :x: Add test for initializing Config

--- config_test.go
+++ config_test.go
@@ -1,1 +1,15 @@
 package daffodil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConfig(t *testing.T) {
+	want := &Config{}
+	got, err := NewConfig()
+
+	assert.IsTypef(t, want, got, "Could not initialize Config")
+	assert.Nil(t, err)
+}
