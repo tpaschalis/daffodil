b9443131c8a64d08eacea0f395d12dec90ad8a95
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-04-30 02:38:34 +0300

    :white_check_mark: Add dummy nodeID field

--- config.go
+++ config.go
@@ -1,9 +1,13 @@
 package daffodil
 
 // Config holds configuration values for building a Daffodil
-type Config struct{}
+type Config struct {
+	nodeID uint16
+}
 
 // NewConfig initializes a Config struct
 func NewConfig() (*Config, error) {
-	return &Config{}, nil
+	return &Config{
+		nodeID: 0,
+	}, nil
 }
