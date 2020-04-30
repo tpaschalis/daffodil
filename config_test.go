12cacd5b233bb6a59055571c18b2a867a68a53a3
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-04-30 14:37:40 +0300

    :x: Add test for transforming an IP into a 16-bit mnode ID

--- config_test.go
+++ config_test.go
@@ -1,6 +1,7 @@
 package daffodil
 
 import (
+	"net"
 	"testing"
 	"time"
 
@@ -11,8 +12,8 @@ func TestNewConfig(t *testing.T) {
 	want := &Config{}
 	got, err := NewConfig()
 
-	assert.IsTypef(t, got.nodeID, uint16(0), "NodeID should be a 16-bit integer")
-	assert.IsTypef(t, got.epoch, time.Now(), "Epoch should be a time.Time object")
+	assert.IsTypef(t, uint16(0), got.nodeID, "NodeID should be a 16-bit integer")
+	assert.IsTypef(t, time.Now(), got.epoch, "Epoch should be a time.Time object")
 	assert.IsTypef(t, want, got, "Could not initialize Config")
 	assert.Nil(t, err)
 }
@@ -22,6 +23,16 @@ func TestHashString(t *testing.T) {
 	got := hashTo16Bits(s)
 	want := uint16(43507)
 
-	assert.IsTypef(t, got, uint16(0), "Hash should be a 16-bit integer")
-	assert.Equalf(t, want, got, "Hashed string should return %v, instead got %v", got, want)
+	assert.IsTypef(t, uint16(0), got, "Hash should be a 16-bit integer")
+	assert.Equalf(t, want, got, "Hashed string should return %v, instead got %v", want, got)
+}
+
+func TestIPTo16Bits(t *testing.T) {
+	ip := net.ParseIP("10.0.1.42")
+	got := ipTo16Bits(ip)
+	want := "foo"
+
+	assert.IsTypef(t, uint16(0), got, "Result should be a 16-bit integer")
+	assert.Equalf(t, want, got, "Transformed IP should return %v, instead got %v", want, got)
+
 }
