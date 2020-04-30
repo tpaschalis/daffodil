e1e7328c2724c729f546ced82b04f07580b1f2c0
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-04-30 13:45:24 +0300

    :white_check_mark: Add ID type and Next method

--- daffodil.go
+++ daffodil.go
@@ -13,7 +13,15 @@ const (
 // Daffodil is an id generator
 type Daffodil struct{}
 
+// ID represents the generated 64-bit UID
+type ID uint64
+
 // NewDaffodil returns a new instance of an ID generator
 func NewDaffodil(cfg Config) (*Daffodil, error) {
 	return &Daffodil{}, nil
+}
+
+// Next generates the next uid
+func (d *Daffodil) Next() ID {
+	return 0
 }
