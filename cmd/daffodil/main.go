7229eac4bb8532306b05c3cae0ca16fb0eb346ec
Paschalis Tsilias <p.tsilias@thebeat.co>
2020-05-03 15:39:56 +0300

    :white_check_mark: Add handlers for generating and dismantling IDs

--- cmd/daffodil/main.go
+++ cmd/daffodil/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/tpaschalis/daffodil"
 )
@@ -11,12 +14,19 @@ var cfg *daffodil.Config
 var df *daffodil.Daffodil
 
 func main() {
-	cfg, _ = daffodil.NewConfig()
-	df, _ = daffodil.NewDaffodil(cfg)
+	cfg, err := daffodil.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	df, err = daffodil.NewDaffodil(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	http.HandleFunc("/", daffodilHandler)
+	http.HandleFunc("/dismantle", dismantleHandler)
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func daffodilHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,8 +35,42 @@ func daffodilHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusLocked)
 		_, err := fmt.Fprint(w, err.Error())
 		if err != nil {
-			fmt.Fprint(w, "Fprint returned error in Write to ResponseWriter : %s", err.Error())
+			fmt.Fprintf(w, "fprint returned error in Write to ResponseWriter : %s", err.Error())
 		}
 	}
+
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%v", id)
+}
+
+func dismantleHandler(w http.ResponseWriter, r *http.Request) {
+	strID := r.URL.Query().Get("id")
+
+	if strID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "url parameter 'id' is missing")
+	}
+
+	id, err := strconv.ParseUint(strID, 10, 64)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "provided 'id' could not be parsed into a Uint64 variable")
+	}
+
+	ts, node, seq := daffodil.ID(id).Dismantle()
+	res, err := json.MarshalIndent(map[string]int64{
+		"id":        int64(id),
+		"timestamp": ts,
+		"node":      node,
+		"sequence":  seq,
+	}, "", "    ")
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "could not generate JSON response")
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(res))
 }
